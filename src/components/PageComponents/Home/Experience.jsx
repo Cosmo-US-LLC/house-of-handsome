e233b2858378417c5ad9573ad0fe4ef7e06e2aa3
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2025-11-11 17:12:19 +0500

    update experience section

--- src/components/PageComponents/Home/Experience.jsx
+++ src/components/PageComponents/Home/Experience.jsx
@@ -7,13 +7,13 @@ import experienceImage4 from "../../../assets/images/experience/exp_c4.png";
 
 function Experience() {
   return (
-    <section className="relative pb-16 w-full bg-white pt-30">
+    <section className="relative pt-14 pb-10 w-full bg-white md:pb-16 md:pt-30">
       {/* Max Container Wrapper - 1280px */}
       <div className="mx-auto max-w-[1280px] px-4 md:px-8 ">
         {/* Arc Images Container */}
         <div className="relative mx-auto mb-12 h-[300px] w-full max-w-[1280px] md:h-[400px]">
           {/* Bottom Left Image */}
-          <div className="absolute bottom-[-160px] left-0 h-[250px] w-[200px] md:h-[393px] md:w-[305px]">
+          <div className="absolute md:bottom-[-160px] bottom-[120px] left-0 h-[124px] w-[94px] md:h-[393px] md:w-[305px]">
             <img
               src={experienceImage1}
               alt="Barber styling client"
@@ -22,7 +22,7 @@ function Experience() {
           </div>
 
           {/* Top Left Image */}
-          <div className="absolute left-[15%] top-0 h-[200px] w-[250px]  md:top-[-33px] md:left-[16.43%] md:h-[312px] md:w-[403px]">
+          <div className="absolute left-[16.5%] top-[-5px] h-[100px] w-[120px]  md:top-[-33px] md:left-[16.43%] md:h-[312px] md:w-[403px]">
             <img
               src={experienceImage2}
               alt="Salon interior with barbers"
@@ -31,7 +31,7 @@ function Experience() {
           </div>
 
           {/* Top Right Image */}
-          <div className="absolute right-[15%] top-0 h-[200px] w-[250px] md:top-[-30px] md:right-[14.76%] md:h-[326px] md:w-[404px]">
+          <div className="absolute right-[16.5%] top-[-3px] h-[100px] w-[106px] md:top-[-30px] md:right-[14.76%] md:h-[326px] md:w-[404px]">
             <img
               src={experienceImage3}
               alt="Man getting professional haircut"
@@ -40,13 +40,16 @@ function Experience() {
           </div>
 
           {/* Bottom Right Image */}
-          <div className="absolute bottom-[-170px] right-0 h-[250px] w-[190px] md:right-[8px] md:h-[380px] md:w-[300px]">
+          <div className="absolute md:bottom-[-170px] bottom-[120px] right-0 h-[116px] w-[90px] md:right-[8px] md:h-[380px] md:w-[300px]">
             <img
               src={experienceImage4}
               alt="Man getting beard shaved"
               className="h-full w-full rounded-[40px] object-cover "
             />
           </div>
+          {/* <div className="absolute bottom-[100px] left-[50%] translate-x-[-50%] mt-4 md:hidden">
+            <PrimaryCTA />
+          </div> */}
         </div>
 
         {/* Text Content - Centered */}
