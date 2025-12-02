53ece4a401037fbf320f44a9e517080a65d57ac8
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2025-12-02 11:44:42 +0500

    update button text

--- src/components/PageComponents/Franchise/TakeFirstStep.jsx
+++ src/components/PageComponents/Franchise/TakeFirstStep.jsx
@@ -135,7 +135,7 @@ export default function TakeFirstStep() {
               />
             </div>
 
-            <PrimaryCTA>Book An Appointment</PrimaryCTA>
+            <PrimaryCTA>Submit</PrimaryCTA>
           </form>
         </div>
       </div>
