35e1cf6f9e9663e6e56586ef8d544a1d47738c23
faheem303 <faheemrehmat360@gmail.com>
2025-12-26 20:35:17 +0500

    updated hover state

--- src/components/PageComponents/Franchise/HelpYouSucceed.jsx
+++ src/components/PageComponents/Franchise/HelpYouSucceed.jsx
@@ -61,7 +61,7 @@ export default function HelpYouSucceed({
             {features.slice(0, 3).map((feature, index) => (
               <div
                 key={index}
-                className="flex flex-col hover:shadow-lg gap-6 items-center text-center bg-white rounded-lg px-12 py-8 w-[389px]"
+                className="flex flex-col transition-shadow duration-300 hover:shadow-[0_0_8px_0_rgba(0,0,0,0.15)]  gap-6 items-center text-center bg-white rounded-lg px-12 py-8 w-[389px]"
               >
                 {/* Icon */}
                 <div className="flex items-center justify-center w-20 h-20 rounded-full bg-[rgba(216,32,40,0.1)]">
@@ -90,7 +90,7 @@ export default function HelpYouSucceed({
             {features.slice(3, 5).map((feature, index) => (
               <div
                 key={index}
-                className="flex flex-col gap-6 hover:shadow-lg items-center text-center bg-white rounded-lg px-12 py-8 w-full sm:w-[437px]"
+                className="flex flex-col gap-6 transition-shadow duration-300 hover:shadow-[0_0_8px_0_rgba(0,0,0,0.15)] items-center text-center bg-white rounded-lg px-12 py-8 w-full sm:w-[437px]"
               >
                 {/* Icon */}
                 <div className="flex items-center justify-center w-20 h-20 rounded-full bg-[rgba(216,32,40,0.1)]">
